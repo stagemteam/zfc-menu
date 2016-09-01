4ba9aa65cff342e273a306bb21d1bcdbb1ab2f0e
Vlad Kozak <vk@agere.com.ua>
2016-09-01 18:12:12 +0300

    MenuHelper has been changed

--- src/View/Helper/MenuHelper.php
+++ src/View/Helper/MenuHelper.php
@@ -17,14 +17,54 @@ class MenuHelper extends AbstractHelper
         return $menuService->getMainMenu();
     }
 
-    public function menu() {
+    /*public function menu() {
         $sm = $this->getServiceManager();
         $menuService = $sm->get('MenuService');
         $menu = $menuService->getMenu();
         foreach ($menu as $page) {
-            printf('<li><a href="/%s">%s</a>', $page->getUrl(), $page->getTitle());
+            printf('<li><a href="/%s">%s <span class="caret"></span></a><ul><li><a href="/patient">Пациенты</a></li></ul></li>', $page->getUrl(), $page->getTitle());
+        }
+    }*/
+
+    public function menu()
+    {
+        $sm = $this->getServiceManager();
+        $menuService = $sm->get('MenuService');
+        $menu = $menuService->getMenu();
+        foreach ($menu as $page) {
+            $children = $menuService->getChildren($page);
+            $class = "";
+            if (!$children) {
+                if ($page->getParent()) {
+                    continue;
+                }
+                printf(
+                    '<li class="">' . "\n",
+                    ($class ? ' ' . $class : '')
+                );
+                printf(
+                    '<a href="/%s">%s</a>' . "\n",
+                    $page->getUrl(),
+                    $page->getTitle()
+                );
+            } else {
+                printf(
+                    '<li class="dropdown%s">' . "\n",
+                    ($class ? ' ' . $class : '')
+                );
+                printf(
+                    '<a href="/" class="sidenav-dropdown-toggle">%s<b class="caret"></b></a>' . "\n",
+                    $page->getTitle()
+                );
+                echo '<ul class="sub-menu">' . "\n";
+                foreach ($children as $child) {
+                    printf('<li><a href="/%s">%s</a></li>', $child->getUrl(), $child->getTitle());
+                }
+                echo "</ul></li>\n";
+            }
         }
     }
+
 
     /*public function menu()
     {
